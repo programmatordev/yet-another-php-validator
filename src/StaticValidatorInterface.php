2dc872e04e0af2bcbea09e88f5cd1005a012a224
André Pimpão <a.pimpao@programmator.dev>
2023-08-01 15:30:01 +0100

    feat: added Choice rule

--- src/StaticValidatorInterface.php
+++ src/StaticValidatorInterface.php
@@ -9,4 +9,6 @@ interface StaticValidatorInterface
     public static function greaterThan(mixed $constraint, array $options = []): ChainedValidatorInterface;
 
     public static function lessThan(mixed $constraint, array $options = []): ChainedValidatorInterface;
+
+    public static function choice(array $constraints, bool $isMultiple = false, ?int $minConstraint = null, ?int $maxConstraint = null, array $options = []): ChainedValidatorInterface;
 }
