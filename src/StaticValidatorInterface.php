0c09bf52bc7d0eea54e654bcc3a0b8a494619ec8
André Pimpão <a.pimpao@programmator.dev>
2023-08-11 18:03:35 +0100

    fix: static and chained validator interfaces

--- src/StaticValidatorInterface.php
+++ src/StaticValidatorInterface.php
@@ -6,21 +6,52 @@ use ProgrammatorDev\YetAnotherPhpValidator\Rule\RuleInterface;
 
 interface StaticValidatorInterface
 {
-    public static function all(array $constraints, array $options = null): ChainedValidatorInterface;
+    public static function all(
+        array $constraints,
+        string $message = 'At "{{ key }}": {{ message }}'
+    ): ChainedValidatorInterface;
 
-    public static function choice(array $constraints, bool $multiple = false, ?int $minConstraint = null, ?int $maxConstraint = null, array $options = []): ChainedValidatorInterface;
+    public static function choice(
+        array $constraints,
+        bool $multiple = false,
+        ?int $minConstraint = null,
+        ?int $maxConstraint = null,
+        string $message = 'The "{{ name }}" value is not a valid choice, "{{ value }}" given. Accepted values are: "{{ constraints }}".',
+        string $multipleMessage = 'The "{{ name }}" value has one or more invalid choices, "{{ value }}" given. Accepted values are: "{{ constraints }}".',
+        string $minMessage = 'The "{{ name }}" value must have at least {{ minConstraint }} choices, {{ numValues }} choices given.',
+        string $maxMessage = 'The "{{ name }}" value must have at most {{ maxConstraint }} choices, {{ numValues }} choices given.'
+    ): ChainedValidatorInterface;
 
-    public static function greaterThan(mixed $constraint, array $options = []): ChainedValidatorInterface;
+    public static function greaterThan(
+        mixed $constraint,
+        string $message = 'The "{{ name }}" value should be greater than "{{ constraint }}", "{{ value }}" given.'
+    ): ChainedValidatorInterface;
 
-    public static function greaterThanOrEqual(mixed $constraint, array $options = []): ChainedValidatorInterface;
+    public static function greaterThanOrEqual(
+        mixed $constraint,
+        string $message = 'The "{{ name }}" value should be greater than or equal to "{{ constraint }}", "{{ value }}" given.'
+    ): ChainedValidatorInterface;
 
-    public static function lessThan(mixed $constraint, array $options = []): ChainedValidatorInterface;
+    public static function lessThan(
+        mixed $constraint,
+        string $message = 'The "{{ name }}" value should be less than "{{ constraint }}", "{{ value }}" given.'
+    ): ChainedValidatorInterface;
 
-    public static function lessThanOrEqual(mixed $constraint, array $options = []): ChainedValidatorInterface;
+    public static function lessThanOrEqual(
+        mixed $constraint,
+        string $message = 'The "{{ name }}" value should be less than or equal to "{{ constraint }}", "{{ value }}" given.'
+    ): ChainedValidatorInterface;
 
-    public static function notBlank(array $options = []): ChainedValidatorInterface;
+    public static function notBlank(
+        ?callable $normalizer = null,
+        string $message = 'The "{{ name }}" value should not be blank, "{{ value }}" given.'
+    ): ChainedValidatorInterface;
 
-    public static function range(mixed $minConstraint, mixed $maxConstraint, array $options = []): ChainedValidatorInterface;
+    public static function range(
+        mixed $minConstraint,
+        mixed $maxConstraint,
+        string $message = 'The "{{ name }}" value should be between "{{ minConstraint }}" and "{{ maxConstraint }}", "{{ value }}" given.'
+    ): ChainedValidatorInterface;
 
     public static function rule(RuleInterface $constraint): ChainedValidatorInterface;
 }
